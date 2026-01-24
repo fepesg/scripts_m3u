d3943f947f85f0a60a001419344c29cf69a52b3b
fepesg <59537897+fepesg@users.noreply.github.com>
2026-01-24 03:52:29 +0100

    Update URL replacement in update_m3u.sh

--- update_m3u.sh
+++ update_m3u.sh
@@ -7,7 +7,7 @@ URL="https://ipfs.io/ipns/k2k4r8oqlcjxsritt5mczkcn4mmvcmymbqw7113fz2flkrerfwfps0
 OUTPUT="/iptv/listaNewEra.m3u"
 
 # Descargar y cambiar el puerto
-curl -s "$URL" | sed 's/:6878/:8000/g' > "$OUTPUT"
+curl -s "$URL" | sed 's/127.0.0.1:6878/Orchestrator:8000/g' > "$OUTPUT"
 
 
 echo "Archivo modificado guardado en: $OUTPUT"
